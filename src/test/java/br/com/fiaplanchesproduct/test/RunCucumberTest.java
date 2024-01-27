aa15eeb855bbf0791b7d1412b85d1dd9a377b0e1
Daniel Tavares <daniel.tavares@xpi.com.br>
2024-01-27 17:34:07 -0300

    feat: added system-test profile

--- src/test/java/br/com/fiaplanchesproduct/test/RunCucumberTest.java
+++ src/test/java/br/com/fiaplanchesproduct/test/RunCucumberTest.java
@@ -1,17 +1,13 @@
 package br.com.fiaplanchesproduct.test;
 
 
-import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import java.io.File;
-
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -21,9 +17,4 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-reports.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.com.fiaplanchesproduct.test.glue")
 public class RunCucumberTest {
-    @Test
-    void testSuite() {
-        File bddResourcesDirectory = new File("src/test/resources/bdd");
-        assertTrue(bddResourcesDirectory.exists());
-    }
 }
--- src/test/resources/bdd/Product.feature
+++ src/test/resources/bdd/Product.feature
@@ -12,17 +12,6 @@ Funcionalidade: Criar Produto na base de Dados
     Quando for realizada a chamada no endpoint de busca de produto por categoria
     Entao Eu recebo uma resposta correta
 
-  Cenario: : 03 - Atualizar cliente na base com sucesso
-    Dado o cliente de CPF 38037984850 e nome Daniel
-    Quando for realizada a chamada no endpoint de busca de cliente
-    E alterar os dados para CPF 38037984850 e nome Cleiton
-    Quando for realizada a chamada no endpoint de atualizacao de cliente
-    Entao o cliente deve ser atualizado com sucesso na base
-
-  Cenario: : 04 - Delecao de cliente na base com sucesso
-    Dado o cliente de CPF 38037984850 e nome Daniel
-    Quando for realizada a chamada no endpoint de delecao de cliente
-    Entao o cliente não deve ser encontrado na base
 
 
 
